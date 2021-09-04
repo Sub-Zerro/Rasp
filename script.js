fd60af61b505af4c81769b25b2015079e259f0e5
Sub-Zerro <85824405+Sub-Zerro@users.noreply.github.com>
2021-09-04 12:56:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,404 @@
+console.log("Подключено");
+
+// Че же написать
+
+
+//6а
+let sixA1 = document.querySelectorAll("th, td");
+sixA1 = sixA1[15];
+console.log(sixA1);
+
+//6б
+let sixB1 = document.querySelectorAll("th, td");
+sixB1 = sixB1[16];
+console.log(sixB1);
+
+//6в
+let sxiV1 = document.querySelectorAll("th, td");
+sxiV1 = sxiV1[17];
+console.log(sxiV1);
+
+//7a
+let sevenA1 = document.querySelectorAll("th, td");
+sevenA1 = sevenA1[18];
+console.log(sevenA1);
+
+//7б
+let sevenB1 = document.querySelectorAll("th, td");
+sevenB1 = sevenB1[19];
+console.log(sevenB1);
+
+//7в
+let sevenV1 = document.querySelectorAll("th, td");
+sevenV1 = sevenV1[20];
+console.log(sevenV1);
+
+//8а
+let eightA1 = document.querySelectorAll("th, td");
+eightA1 = eightA1[21];
+console.log(eightA1);
+
+//8б
+let eightB1 = document.querySelectorAll("th, td");
+eightB1 = eightB1[22];
+console.log(eightB1);
+
+//8в
+let eightV1 = document.querySelectorAll("th, td");
+eightV1 = eightV1[23];
+console.log(eightV1);
+
+
+
+// Вторая строка
+
+
+
+//6а
+let sixA2 = document.querySelectorAll("th, td");
+sixA2 = sixA2[26];
+console.log(sixA2);
+
+//6б
+let sixB2 = document.querySelectorAll("th, td");
+sixB2 = sixB2[27];
+console.log(sixB2);
+
+//6в
+let sxiV2 = document.querySelectorAll("th, td");
+sxiV2 = sxiV2[28];
+console.log(sxiV2);
+
+//7a
+let sevenA2 = document.querySelectorAll("th, td");
+sevenA2 = sevenA2[29];
+console.log(sevenA2);
+
+//7б
+let sevenB2 = document.querySelectorAll("th, td");
+sevenB2 = sevenB2[30];
+console.log(sevenB2);
+
+//7в
+let sevenV2 = document.querySelectorAll("th, td");
+sevenV2 = sevenV2[31];
+console.log(sevenV2);
+
+//8а
+let eightA2 = document.querySelectorAll("th, td");
+eightA2 = eightA2[32];
+console.log(eightA2);
+
+//8б
+let eightB2 = document.querySelectorAll("th, td");
+eightB2 = eightB2[33];
+console.log(eightB2);
+
+//8в
+let eightV2 = document.querySelectorAll("th, td");
+eightV2 = eightV2[34];
+console.log(eightV2);
+
+
+
+// Третяяя строка
+
+
+
+//6а
+let sixA3 = document.querySelectorAll("th, td");
+sixA3 = sixA3[37];
+console.log(sixA3);
+
+//6б
+let sixB3 = document.querySelectorAll("th, td");
+sixB3 = sixB3[38];
+console.log(sixB3);
+
+//6в
+let sxiV3 = document.querySelectorAll("th, td");
+sxiV3 = sxiV3[39];
+console.log(sxiV3);
+
+//7a
+let sevenA3 = document.querySelectorAll("th, td");
+sevenA3 = sevenA3[40];
+console.log(sevenA3);
+
+//7б
+let sevenB3 = document.querySelectorAll("th, td");
+sevenB3 = sevenB3[41];
+console.log(sevenB3);
+
+//7в
+let sevenV3 = document.querySelectorAll("th, td");
+sevenV3 = sevenV3[42];
+console.log(sevenV3);
+
+//8а
+let eightA3 = document.querySelectorAll("th, td");
+eightA3 = eightA3[43];
+console.log(eightA3);
+
+//8б
+let eightB3 = document.querySelectorAll("th, td");
+eightB3 = eightB3[44];
+console.log(eightB3);
+
+//8в
+let eightV3 = document.querySelectorAll("th, td");
+eightV3 = eightV3[45];
+console.log(eightV3);
+
+
+
+// Четвертая строка
+
+
+
+//6а
+let sixA4 = document.querySelectorAll("th, td");
+sixA4 = sixA4[48];
+console.log(sixA4);
+
+//6б
+let sixB4 = document.querySelectorAll("th, td");
+sixB4 = sixB4[49];
+console.log(sixB4);
+
+//6в
+let sxiV4 = document.querySelectorAll("th, td");
+sxiV4 = sxiV4[50];
+console.log(sxiV4);
+
+//7a
+let sevenA4 = document.querySelectorAll("th, td");
+sevenA4 = sevenA4[51];
+console.log(sevenA4);
+
+//7б
+let sevenB4 = document.querySelectorAll("th, td");
+sevenB4 = sevenB4[52];
+console.log(sevenB4);
+
+//7в
+let sevenV4 = document.querySelectorAll("th, td");
+sevenV4 = sevenV4[53];
+console.log(sevenV4);
+
+//8а
+let eightA4 = document.querySelectorAll("th, td");
+eightA4 = eightA4[54];
+console.log(eightA4);
+
+//8б
+let eightB4 = document.querySelectorAll("th, td");
+eightB4 = eightB4[55];
+console.log(eightB4);
+
+//8в
+let eightV4 = document.querySelectorAll("th, td");
+eightV4 = eightV4[56];
+console.log(eightV4);
+
+
+// Пятая строка
+
+
+//6а
+let sixA5 = document.querySelectorAll("th, td");
+sixA5 = sixA5[59];
+console.log(sixA5);
+
+//6б
+let sixB5 = document.querySelectorAll("th, td");
+sixB5 = sixB5[60];
+console.log(sixB5);
+
+//6в
+let sxiV5 = document.querySelectorAll("th, td");
+sxiV5 = sxiV5[61];
+console.log(sxiV5);
+
+//7a
+let sevenA5 = document.querySelectorAll("th, td");
+sevenA5 = sevenA5[62];
+console.log(sevenA5);
+
+//7б
+let sevenB5 = document.querySelectorAll("th, td");
+sevenB5 = sevenB5[63];
+console.log(sevenB5);
+
+//7в
+let sevenV5 = document.querySelectorAll("th, td");
+sevenV5 = sevenV5[64];
+console.log(sevenV5);
+
+//8а
+let eightA5 = document.querySelectorAll("th, td");
+eightA5 = eightA5[65];
+console.log(eightA5);
+
+//8б
+let eightB5 = document.querySelectorAll("th, td");
+eightB5 = eightB5[66];
+console.log(eightB5);
+
+//8в
+let eightV5 = document.querySelectorAll("th, td");
+eightV5 = eightV5[67];
+console.log(eightV5);
+
+
+// Шестая строка
+
+
+//6а
+let sixA6 = document.querySelectorAll("th, td");
+sixA6 = sixA6[70];
+console.log(sixA6);
+
+//6б
+let sixB6 = document.querySelectorAll("th, td");
+sixB6 = sixB6[71];
+console.log(sixB6);
+
+//6в
+let sxiV6 = document.querySelectorAll("th, td");
+sxiV6 = sxiV6[72];
+console.log(sxiV6);
+
+//7a
+let sevenA6 = document.querySelectorAll("th, td");
+sevenA6 = sevenA6[73];
+console.log(sevenA6);
+
+//7б
+let sevenB6 = document.querySelectorAll("th, td");
+sevenB6 = sevenB6[74];
+console.log(sevenB6);
+
+//7в
+let sevenV6 = document.querySelectorAll("th, td");
+sevenV6 = sevenV6[75];
+console.log(sevenV6);
+
+//8а
+let eightA6 = document.querySelectorAll("th, td");
+eightA6 = eightA6[76];
+console.log(eightA6);
+
+//8б
+let eightB6 = document.querySelectorAll("th, td");
+eightB6 = eightB6[77];
+console.log(eightB6);
+
+//8в
+let eightV6 = document.querySelectorAll("th, td");
+eightV6 = eightV6[78];
+console.log(eightV6);
+
+
+// Седьмая строка
+
+
+//6а
+let sixA7 = document.querySelectorAll("th, td");
+sixA7 = sixA7[81];
+console.log(sixA7);
+
+//6б
+let sixB7 = document.querySelectorAll("th, td");
+sixB7 = sixB7[82];
+console.log(sixB7);
+
+//6в
+let sxiV7 = document.querySelectorAll("th, td");
+sxiV7 = sxiV7[83];
+console.log(sxiV7);
+
+//7a
+let sevenA7 = document.querySelectorAll("th, td");
+sevenA7 = sevenA7[84];
+console.log(sevenA7);
+
+//7б
+let sevenB7 = document.querySelectorAll("th, td");
+sevenB7 = sevenB7[85];
+console.log(sevenB7);
+
+//7в
+let sevenV7 = document.querySelectorAll("th, td");
+sevenV7 = sevenV7[86];
+console.log(sevenV7);
+
+//8а
+let eightA7 = document.querySelectorAll("th, td");
+eightA7 = eightA7[87];
+console.log(eightA7);
+
+//8б
+let eightB7 = document.querySelectorAll("th, td");
+eightB7 = eightB7[88];
+console.log(eightB7);
+
+//8в
+let eightV7 = document.querySelectorAll("th, td");
+eightV7 = eightV7[89];
+console.log(eightV7);
+
+
+// Седьмая строка
+
+
+//6а
+let sixA8 = document.querySelectorAll("th, td");
+sixA8 = sixA8[92];
+console.log(sixA8);
+
+//6б
+let sixB8 = document.querySelectorAll("th, td");
+sixB8 = sixB8[93];
+console.log(sixB8);
+
+//6в
+let sxiV8 = document.querySelectorAll("th, td");
+sxiV8 = sxiV8[94];
+console.log(sxiV8);
+
+//7a
+let sevenA8 = document.querySelectorAll("th, td");
+sevenA8 = sevenA8[95];
+console.log(sevenA8);
+
+//7б
+let sevenB8 = document.querySelectorAll("th, td");
+sevenB8 = sevenB8[96];
+console.log(sevenB8);
+
+//7в
+let sevenV8 = document.querySelectorAll("th, td");
+sevenV8 = sevenV8[97];
+console.log(sevenV8);
+
+//8а
+let eightA8 = document.querySelectorAll("th, td");
+eightA8 = eightA8[98];
+console.log(eightA8);
+
+//8б
+let eightB8 = document.querySelectorAll("th, td");
+eightB8 = eightB8[99];
+console.log(eightB8);
+
+//8в
+let eightV8 = document.querySelectorAll("th, td");
+eightV8 = eightV8[100];
+console.log(eightV8);
+
+
+
+
+
+
